89c17c42d28d1678263b664698682fc33165363a
vitor-apolinario <vitorapoli@gmail.com>
2019-09-02 17:16:38 -0300

    configs db

--- queries.js
+++ queries.js
@@ -1,9 +1,9 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'api',
-  password: 'postgres',
+  user: 'ntooewegrdsppa',
+  host: 'ec2-54-83-33-14.compute-1.amazonaws.com',
+  database: 'd95i0qida38ocu',
+  password: 'f3f16e6612f7c15af4639553e8fe9874d8200954a2cfa22e895040bd5eff7733',
   port: 5432,
 })
 

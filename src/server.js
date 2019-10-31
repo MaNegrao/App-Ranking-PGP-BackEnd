67fc74a86ac0996d26110c5aa8813206edb5582b
vitor-apolinario <vitorapoli@gmail.com>
2019-10-31 15:30:50 -0300

    linting server.js

--- src/server.js
+++ src/server.js
@@ -1,19 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 
-const app = express()
-var port = process.env.PORT || 8080;
+const app = express();
+const port = process.env.PORT || 8080;
 
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
 
 app.get('/', (request, response) => {
-    response.json({ info: 'Node.js, Express, and Postgres Truco UFFS API' })
-})
+  response.json({ info: 'Node.js, Express, and Postgres Truco UFFS API' });
+});
 
 // quaquer rota que inicie com /api, é redirecionada p/ routes
-app.use('/api', require('./routes'))
+app.use('/api', require('./routes'));
 
-app.listen(port, () => {
-    console.log(`App running on port ${port}.`)
-})
+app.listen(port);

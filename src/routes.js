29413ea066a1a8b866c09d9248757e025a54a579
vitor-apolinario <vitorapoli@gmail.com>
2019-11-30 17:23:39 -0300

    cleaning repo

--- src/routes.js
+++ src/routes.js
@@ -8,10 +8,6 @@ const PlayerController = require('./controllers/PlayerController');
 const SessionController = require('./controllers/SessionController');
 const SearchController = require('./controllers/SearchController');
 const GameController = require('./controllers/GameController');
-/*
-const TeamController = require('./controllers/TeamController');
-cost MatchController = require('./controllers/MatchController');
-*/
 
 routes.post('/players', PlayerController.create);
 routes.post('/authenticate', SessionController.authenticate);
@@ -22,30 +18,5 @@ routes.use(authMiddleware);
 routes.get('/search/:user_string', SearchController.search);
 
 routes.post('/endgame', GameController.storeResult);
-
-/*
-routes.get('/players', PlayerController.index)
-routes.get('/players/:nick', PlayerController.read)
-routes.put('/players/:nick', PlayerController.update)
-routes.delete('/players/:nick', PlayerController.delete)
-
-routes.get('/teams', TeamController.index)
-routes.post('/teams', TeamController.create)
-routes.get('/teams/:id', TeamController.read)
-routes.put('/teams/:id', TeamController.update)
-routes.delete('/teams/:id', TeamController.delete)
-
-routes.get('/plays', TeamController.index)
-routes.post('/plays', TeamController.create)
-routes.get('/plays/:id', TeamController.read)
-routes.put('/plays/:id', TeamController.update)
-routes.delete('/plays/:id', TeamController.delete)
-
-routes.get('/matches', MatchController.index)
-routes.post('/matches', MatchController.create)
-routes.get('/matches/:id', MatchController.read)
-routes.put('/matches/:id', MatchController.update)
-routes.delete('/matches/:id', MatchController.delete)
-*/
 
 module.exports = routes;
